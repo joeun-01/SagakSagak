4ae8de90b4f4b98bc8d430f9316a558c9246ad99
YSH <sh10302@naver.com>
2022-05-22 08:11:10 +0900

    홈 화면 세부 프래그먼트 구성

--- app/src/main/java/com/songilcraft/sagak_sagak/MainActivity.kt
+++ app/src/main/java/com/songilcraft/sagak_sagak/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.songilcraft.sagak_sagak
 
+import android.content.Intent
 import android.os.Bundle
 import com.songilcraft.sagak_sagak.config.BaseActivity
 import com.songilcraft.sagak_sagak.databinding.ActivityMainBinding
+import com.songilcraft.sagak_sagak.screen.gallery.GalleryActivity
 import com.songilcraft.sagak_sagak.screen.home.HomeFragment
+import com.songilcraft.sagak_sagak.screen.profile.ProfileFragment
+import com.songilcraft.sagak_sagak.screen.storywrite.StoryWriteActivity
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
@@ -30,20 +34,34 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                 }
 
                 R.id.addFragment -> {
-                    supportFragmentManager.beginTransaction()
+                    /*supportFragmentManager.beginTransaction()
                         .replace(R.id.layout_fragment, HomeFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
+                        .commitAllowingStateLoss()*/
+                    startActivity(Intent(this, StoryWriteActivity::class.java))
+                    return@setOnItemSelectedListener false
                 }
 
                 R.id.profileFragment -> {  // 검색 화면 실행
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.layout_fragment, HomeFragment())
+                        .replace(R.id.layout_fragment, ProfileFragment())
                         .commitAllowingStateLoss()
                     return@setOnItemSelectedListener true
                 }
             }
             false
         }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            1000 -> {
+                moveToImagePicker()
+            }
+        }
+    }
+
+    private fun moveToImagePicker() {
+        startActivity(Intent(this, GalleryActivity::class.java))
     }
 }
