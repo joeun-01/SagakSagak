b3e7280c3dff969980b50362e210f19c8c94524e
YSH <sh10302@naver.com>
2022-05-21 23:58:48 +0900

    ticket 관련 레이아웃 및 테스트 데이터 형식 추가

---
+++ app/src/main/java/com/songilcraft/sagak_sagak/data/TestTicket.kt
@@ -0,0 +1,3 @@
+package com.songilcraft.sagak_sagak.data
+
+data class TestTicket(val imageUrl: String = "https://songil-prod.s3.ap-northeast-2.amazonaws.com/craft/%EC%9E%91%EC%95%BD%ED%94%8C%EB%A0%88%EC%9D%B4%ED%8A%B8+%EC%86%8C+%EB%A9%94%EC%9D%B8%EC%82%AC%EC%A7%84.jpg", val title : String = "타이틀", val date : String = "2022.05.21")
---
+++ app/src/main/java/com/songilcraft/sagak_sagak/recycler/adapter/TicketAdapter.kt
@@ -0,0 +1,31 @@
+package com.songilcraft.sagak_sagak.recycler.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.songilcraft.sagak_sagak.data.TestTicket
+import com.songilcraft.sagak_sagak.databinding.ViewTicketBinding
+import com.songilcraft.sagak_sagak.recycler.viewholder.TicketViewHolder
+
+class TicketAdapter : RecyclerView.Adapter<TicketViewHolder>(){
+
+    private val ticketList = arrayListOf<TestTicket>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TicketViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ViewTicketBinding.inflate(inflater, parent, false)
+        return TicketViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TicketViewHolder, position: Int) {
+        holder.bind(ticketList[position])
+    }
+
+    override fun getItemCount(): Int = ticketList.size
+
+    fun applyData(newTicketList : ArrayList<TestTicket>){
+        ticketList.clear()
+        ticketList.addAll(newTicketList)
+        notifyItemRangeInserted(0, newTicketList.size)
+    }
+}
