88d08e33fd693e5470c95b4bb1cc82f94c57457d
YSH <sh10302@naver.com>
2022-05-22 08:09:33 +0900

    스플래시 화면에서 자동 로그인 적용

--- app/src/main/java/com/songilcraft/sagak_sagak/screen/start/StartActivity.kt
+++ app/src/main/java/com/songilcraft/sagak_sagak/screen/start/StartActivity.kt
@@ -7,15 +7,23 @@ import android.os.Looper
 import com.songilcraft.sagak_sagak.MainActivity
 import com.songilcraft.sagak_sagak.R
 import com.songilcraft.sagak_sagak.config.BaseActivity
+import com.songilcraft.sagak_sagak.config.GlobalApplication
 import com.songilcraft.sagak_sagak.databinding.ActivityStartBinding
+import com.songilcraft.sagak_sagak.screen.login.LoginActivity
 
 class StartActivity : BaseActivity<ActivityStartBinding>(R.layout.activity_start){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            val userIdx = GlobalApplication.globalSharedPreferences.getInt(GlobalApplication.USER_IDX, -1)
+            if (userIdx != -1){
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
         }, 1000)
     }
 }
